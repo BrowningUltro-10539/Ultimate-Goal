2668970dbc3dc8675f1e568fe8da7ac96a25b3e7
washieuanan <wanan@teakfellowship.org>
2021-03-05 13:56:23 -0500

    CVPipeline Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RingDeterminationPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RingDeterminationPipeline.java
@@ -36,8 +36,8 @@ public class RingDeterminationPipeline extends OpenCvPipeline
      */
     static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(181,98);
 
-    static final int REGION_WIDTH = 35;
-    static final int REGION_HEIGHT = 25;
+    static final int REGION_WIDTH = 45;
+    static final int REGION_HEIGHT = 35;
 
     final int FOUR_RING_THRESHOLD = 150;
     final int ONE_RING_THRESHOLD = 135;

4ed087afacf8de33c9f8e9bc8e88ddbe1f613fa2
ericsigg <esigg@browning.edu>
2021-06-01 13:27:31 -0400

    final championship code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOpMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOpMecanum.java
@@ -23,8 +23,6 @@ public class TeleOpMecanum extends OpMode {
         map.init(hardwareMap);
         telemetry.addData("Status:", " Initialized");
 
-        mdt = new MecanumDriveTrain(this, 0, 0, 0);
-
 //        mdt = new MecanumDriveTrain(LinearOpMode, -50, -5, 0);
 
 
@@ -61,10 +59,33 @@ public class TeleOpMecanum extends OpMode {
         }
 
 
-        if(switchDirection){
-            mdt.setControls(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x);
-        } else {
-            mdt.setControls(gamepad1.left_stick_y, -gamepad1.left_stick_x, gamepad1.right_stick_x);
+        if(switchDirection) {
+            double r = Math.hypot(-gamepad1.left_stick_y, -gamepad1.left_stick_x);
+            double robotAngle = Math.atan2(-gamepad1.left_stick_x, -gamepad1.left_stick_y) - Math.PI / 4;
+            double rightX = gamepad1.right_stick_x;
+            final double v1 = r * Math.cos(robotAngle) + rightX;
+            final double v2 = r * Math.sin(robotAngle) - rightX;
+            final double v3 = r * Math.sin(robotAngle) + rightX;
+            final double v4 = r * Math.cos(robotAngle) - rightX;
+
+            map.getLeftTop().setPower(v1);
+            map.getRightTop().setPower(v2);
+            map.getLeftBottom().setPower(v3);
+            map.getRightBottom().setPower(v4);
+        }else{
+
+            double r = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+            double rightX = gamepad1.right_stick_x;
+            final double v1 = r * Math.cos(robotAngle) + rightX;
+            final double v2 = r * Math.sin(robotAngle) - rightX;
+            final double v3 = r * Math.sin(robotAngle) + rightX;
+            final double v4 = r * Math.cos(robotAngle) - rightX;
+
+            map.getLeftTop().setPower(v1);
+            map.getRightTop().setPower(v2);
+            map.getLeftBottom().setPower(v3);
+            map.getRightBottom().setPower(v4);
         }
 
 
